686c75189038077deffd86bcb7bf6485ed441cd7
Ananda Rodrigues! <ananda.mohini.dasi@hotmail.com>
2019-12-22 19:03:00 -0200

    Add arm to slot machine and activating reel with it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,21 @@ export class AppComponent implements OnInit {
  public rotationStyle = {'transform' :'translateZ(-288px) rotateX(60deg)'};
  public cellCount = 5;
  public selectedIndex = 1;
+ public armClicked = false;
+ public reelSpinning = false;
 
 public nextButton() {
-  //console.log(this.selectedIndex, this.cellCount);
  this.selectedIndex++;
  let angle = this.selectedIndex / this.cellCount * 360;
  this.rotationStyle = {'transform' : `rotateX(${angle}deg)`};  
-console.log(this.rotationStyle);
-
 };
+
+public startReel() {
+  console.log('there we go champion!');
+  this.armClicked = true;
+  setTimeout(()=> this.armClicked = false, 500)
+  this.reelSpinning = true;
+}
 
 ngOnInit() {
 // setInterval(() => this.nextButton(), 500); 
