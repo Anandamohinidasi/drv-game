c53910dc7d1ec6d7aa723cd265a23944db7fe800
Ananda Rodrigues! <ananda.mohini.dasi@hotmail.com>
2019-12-22 17:36:54 -0200

    Replace poor weel effect with a better weel rolling animation on css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'derivco-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+ public rotationStyle = {'transform' :'translateZ(-288px) rotateX(60deg)'};
+ public cellCount = 5;
+ public selectedIndex = 1;
+
+public nextButton() {
+  //console.log(this.selectedIndex, this.cellCount);
+ this.selectedIndex++;
+ let angle = this.selectedIndex / this.cellCount * 360;
+ this.rotationStyle = {'transform' : `rotateX(${angle}deg)`};  
+console.log(this.rotationStyle);
+
+};
+
+ngOnInit() {
+// setInterval(() => this.nextButton(), 500); 
+}
+
 }
